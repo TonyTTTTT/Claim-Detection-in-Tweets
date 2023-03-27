e5143a4cd8640f052e835edc74058604b42c2fd7
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2023-03-27 18:20:13 +0800

    add explain by GPT

--- models/data_preprocess_methods.py
+++ models/data_preprocess_methods.py
@@ -296,10 +296,10 @@ def discard_similar_frame(frames):
 
 
 def summary_by_GPT(ids, topic_ids, texts, labels, dataset):
-    if os.path.exists('preprocess_datasets_tsv/{}_extract_by_GPT_v2.tsv'.format(dataset)):
-        data = pd.read_csv('preprocess_datasets_tsv/{}_extract_by_GPT_v2.tsv'.format(dataset), sep='\t')
+    if os.path.exists('preprocess_datasets_tsv/{}_explain_by_GPT.tsv'.format(dataset)):
+        data = pd.read_csv('preprocess_datasets_tsv/{}_explain_by_GPT.tsv'.format(dataset), sep='\t')
         ids, topic_ids, texts, labels = read_df_to_lists(data)
-        print("load from preprocess_datasets_tsv/{}_extract_by_GPT_v2.tsv".format(dataset))
+        print("load from preprocess_datasets_tsv/{}_explain_by_GPT.tsv".format(dataset))
         return ids, topic_ids, texts, labels
 
     chatgpt = ChatGPT()
@@ -307,7 +307,7 @@ def summary_by_GPT(ids, topic_ids, texts, labels, dataset):
     for i in range(0, len(texts)):
         messages = [
             # {"role": "system", "content": "You are a summarizer"},
-            {"role": "user", "content": texts[i] + '\nPlease extract the claim made in the article above:'},
+            {"role": "user", "content": texts[i] + '\nPlease explain the claim made in the article above:'},
         ]
         res = chatgpt.get_response(messages)
 
@@ -315,7 +315,7 @@ def summary_by_GPT(ids, topic_ids, texts, labels, dataset):
 
     df = pd.DataFrame(list(zip(topic_ids, ids, texts_rewrite, labels)), columns=['topic', 'tweet_id', 'tweet_text', 'class_label'])
     df['tweet_id'] = df['tweet_id'].astype(str)
-    df.to_csv('preprocess_datasets_tsv/{}_extract_by_GPT_v2.tsv'.format(dataset), sep='\t', index=False)
+    df.to_csv('preprocess_datasets_tsv/{}_explain_by_GPT.tsv'.format(dataset), sep='\t', index=False)
 
     return ids, topic_ids, texts_rewrite, labels
 
