208410a64512ecc73fe07917dc393accbfaa805a
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2023-05-20 06:48:25 +0800

    add OC_VG_WTP

--- dataset/other-sentence-level/combine_sentence_level_dataset.py
+++ dataset/other-sentence-level/combine_sentence_level_dataset.py
@@ -8,7 +8,7 @@ df_combined_test = pd.DataFrame()
 cnt_train = 0
 cnt_dev = 0
 cnt_test = 0
-dataset_list = ['OC_processed', 'VG_processed', 'WTP_processed', 'WD_processed']
+dataset_list = ['OC_processed', 'VG_processed', 'WTP_processed']
 
 for path in os.listdir():
     if path in dataset_list:
@@ -31,7 +31,7 @@ for path in os.listdir():
                 cnt_test += df.shape[0]
                 df_combined_test = pd.concat([df_combined_test, df], ignore_index=True)
 
-combined_dataset_name = 'OC_VG_WTP_WD'
+combined_dataset_name = 'OC_VG_WTP'
 df_combined_train.to_csv('sentence_level_{}_train.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
 df_combined_dev.to_csv('sentence_level_{}_dev.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
 df_combined_test.to_csv('sentence_level_{}_test.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
