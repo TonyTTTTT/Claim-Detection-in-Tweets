c75f6e015783f1e4d35c76a75b51851d93cb6a79
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2023-06-09 02:43:08 +0800

    add LESA normalize by GPT

--- models/chatGPT_api.py
+++ models/chatGPT_api.py
@@ -17,12 +17,13 @@ class ChatGPT:
             model="gpt-3.5-turbo",
             messages=messages,
             temperature=0,
+            # max_tokens=1997,
             # top_p=0.01,
         )
         print('=============================')
-        print('GPT input: {}'.format((messages)))
+        print('GPT input:\n{}'.format((messages)))
         print('GPT input #words: {}'.format(len(messages[-1]['content'].split())))
-        print('GPT response: {}'.format(res.choices[0].message.content.strip()))
+        print('GPT response:\n{}'.format(res.choices[0].message.content.strip()))
         print('GPT response #words: {}'.format(len(res.choices[0].message.content.strip().split())))
         print('=============================')
 
@@ -31,7 +32,19 @@ class ChatGPT:
 
 
 if __name__ == '__main__':
-    content = "#Enugu govt takes delivery of #COVID19vaccines Enugu State Government alongside Ebonyi and Anambra states, on Thursday, took delivery of the first batch of #oxfordastrazeneca COVID-19 vaccines from @NphcdaNG, at Enugu Airport... https://t.co/srx1CWZqtK @Coal_City #COVID19 https://t.co/PrO9GSiXuv"
+    tweet1 = "@PrisonPlanet @ezralevant those F3cKTARDS have been eating #BATSOUP and other CRAZY SH!T for centuries. if this #COVID19 #CoronaVIRUS thing happened from that it would have happened by now. this is a fooking #BIOWeapon created in a #CHINA LAB #Agenda21 #Agenda2030= #CHINAISASSHO #UN https://t.co/gSNEWit4EA"
+    tweet2 = "Breaking: obscure law requires Sen confirmation for WH aide like Bannon to serve on NSC. 50 U.S. Code § 3021 https://t.co/1sRQEnP3CY — Jonathan Alter (@jonathanalter) January 31, 2017"
+    tweet3 = "All I'm saying is meth will cure Coronavirus"
+    tweet4 = "Rather than fighting #coronavirus , May be you could use @Lysol to make #TheRealDonaldTrump go away"
+    tweet5 = "\"@Nigel_Farage #Coronavirus was #sars virus! #china synthesized it in a lab to be more deadly, more contagious! Using as bio-weapon worldwide now! To weaken other economies globally!\n#Covid_19\""
+
+    content = tweet5
+    messages_normalize = [
+        {"role": "user", "content": content + "\nnormalize no matter the context:"},
+    ]
+    messages_normalize_v2 = [
+        {"role": "user", "content": content + "\nnormalize the tweet:"},
+    ]
     messages_summary = [
         # {"role": "system", "content": "Please provide a brief summary of the article in no more than 20 words."},
         {"role": "user", "content": content+"\nsummarize:"},
@@ -69,12 +82,27 @@ if __name__ == '__main__':
     ]
     messages_rewrite_v2 = [
         # {"role": "system", "content": "Can you rephrase the following article to be more clear and easy to understand?"},
-        {"role": "user", "content": content + "\nrewrite as a journalist and don't over interpretation:"}
+        {"role": "user", "content": content + "\nrewrite the tweet:"}
+    ]
+
+    article1 = "通告各位好友，我兒已確定:你若有辦敬老卡每人每個月乘坐交通工具補助480元沒用完，"\
+                "可在月底前去便利商店買東西把他用完以免浪費。今天才知道，平白浪費不少!"
+    article2 = '媽媽在醫院支援武漢前線醫護發回的消息，這段期間切記不要穿帶毛領或是絨線的衣服外套，較容易吸附病毒，請廣而告之。'
+    article3 = '有在煮飯的請注意 菌類不能和茄子一起吃，各位注意到了嗎？今年各種蘑菇特別便宜。這是當醫生的同學轉發的，請注意！轉發「 緊急通知：∵醫大已經死17人，友情提醒:最近醫院急診的患者比較多，大都是蘑菇中毒， 今年蘑菇豐收，蘑菇可以和小白菜一起炒,但不能和茄子一起吃，會中毒，在水焯蘑菇的時候放大蒜，如果大蒜變色了，就有毒，不可食用。而且蘑菇和小米、大黃米千萬不要同吃，會產生一種毒素，醫院治不好，後果很嚴重。'
+    article4 = '有在唸大專院校的孩子嗎？昨天教育部針對此次疫情搞了一個紓困措施，只要你的小孩仍就讀大專院校的話，請備妥個人身分證影本、戶口名簿影本、學生自己的銀行帳戶影本，這兩天拿到學務處去申請紓困，有申請就會有（一次3000元，連發三個月共9000元）。不管有沒有受到疫情影響，申請了就有喔！'
+    article5 = '溫哥華回來投票的朋友傳來：請各位告訴你們的親朋好友，我剛剛接到我在桃園的弟弟的Line，告訴我今天才收到投票通知，我們過去十幾年都固定在一個國小投票，今年改在一個完全不一樣的地方，另外不可以帶手機，如果是國外回去的一定要帶台灣護照，以前從來沒有這個規定，請大家一定要非常的注意。如果沒有注意投票的地方，臨時發現時恐怕會卡在來回的交通之中而錯過投票。為什麼國外回來的僑胞投票要帶䕶照去投票？是真的還是假新聞？葉元之議員可否幫大家查證一下。'
+
+    messages_CDDTC = [
+        {"role": "system", "content": "想像你是一個嚴謹的事實查核人員"},
+        {"role": "user", "content": "請將下面訊息中\"可以查核\"且\"值得查核\"的部分挑選出來，並以文中的句子條列:\n" + article1}
     ]
     chatgpt = ChatGPT()
+    # res_normalize = chatgpt.get_response(messages_normalize)
+    # res_normalize_v2 = chatgpt.get_response(messages_normalize_v2)
+
     # res_summary = chatgpt.get_response(messages_summary)
-    res_explain = chatgpt.get_response(messages_explain)
-    res_explain_v2 = chatgpt.get_response(messages_explain_v2)
+    # res_explain = chatgpt.get_response(messages_explain)
+    # res_explain_v2 = chatgpt.get_response(messages_explain_v2)
 
     # res_simplify = chatgpt.get_response(messages_simplify)
     # res_simplify_v2 = chatgpt.get_response(messages_simplify_v2)
@@ -82,7 +110,21 @@ if __name__ == '__main__':
     # res_extract = chatgpt.get_response(messages_extract)
     # res_extract_v2 = chatgpt.get_response(messages_extract_v2)
     # res_extract_v3 = chatgpt.get_response(messages_extract_v3)
+
     messages_rewrite = chatgpt.get_response(messages_rewrite)
-    # messages_rewrite_v2 = chatgpt.get_response(messages_rewrite_v2)
+    messages_rewrite_v2 = chatgpt.get_response(messages_rewrite_v2)
+
+    # messages_CDDTC = chatgpt.get_response(messages_CDDTC)
+
 
     # res_split = res_extract.split()
+
+# openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "想像你是一個嚴謹的事實查核人員"},
+#         {"role": "user", "content": "請將下面訊息中\"可以查核\"的部分挑選出來，並以文中的句子條列:\n"
+#                                     "通告各位好友，我兒已確定:你若有辦敬老卡每人每個月乘坐交通工具補助480元沒用完，"
+#                                     "可在月底前去便利商店買東西把他用完以免浪費。今天才知道，平白浪費不少!"},
+#     ]
+# )
--- models/model_config.py
+++ models/model_config.py
@@ -24,30 +24,30 @@ learning_rate = 5e-7  # local
 # num_train_epochs = 10  # LESA
 # num_train_epochs = 7 # CLEF2022 1b
 # num_train_epochs = 5 # other sentence-level
-num_train_epochs = 1  # local
+num_train_epochs = 5  # local
 
 warm_up_epochs = 1
 
 # ['linear', 'cosine', 'cosine_with_restarts', 'polynomial', 'constant', 'constant_with_warmup']
-lr_scheduler_type = "constant_with_warmup"
+lr_scheduler_type = "linear"
 per_device_train_batch_size = 4
 device_num = 1
 
 dataset_name = 'LESA'
 test_dataset_name = 'LESA'
-tags = ['sentence level']
-run_name = 'baseline_CB_VG_NC_balanced'
+tags = ['LESA']
+run_name = 'local'
 
 seeds = [42, 17, 36]
-preprocess_function = none_operation
+preprocess_function = rewrite_by_GPT
 concate_frames_num = 5
 
-do_balancing = True
+do_balancing = False
 do_normalize = True
 
-delete_tail_hashtag = True
+delete_tail_hashtag = False
 delete_at = False
 delete_hashtag = False
-delete_url = True
-delete_emoji = True
-replace_covid = True
+delete_url = False
+delete_emoji = False
+replace_covid = False
