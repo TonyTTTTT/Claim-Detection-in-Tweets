9391ef3bab76eba415f3191e4ef2d21f7903ea05
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2023-06-15 12:48:36 +0800

    add CB_OC

--- dataset/other-sentence-level/combine_sentence_level_dataset.py
+++ dataset/other-sentence-level/combine_sentence_level_dataset.py
@@ -8,7 +8,7 @@ df_combined_test = pd.DataFrame()
 cnt_train = 0
 cnt_dev = 0
 cnt_test = 0
-dataset_list = ['ClaimBuster_processed', 'VG_processed', 'NewsClaims_processed', 'OC_processed', 'WD_processed', 'WTP_processed', 'MT_processed', 'PE_processed']
+dataset_list = ['ClaimBuster_processed', 'OC_processed']
 
 for path in os.listdir():
     if path in dataset_list:
@@ -32,7 +32,7 @@ for path in os.listdir():
                     cnt_test += df.shape[0]
                     df_combined_test = pd.concat([df_combined_test, df], ignore_index=True)
 
-combined_dataset_name = 'CB_VG_NC_OC_WD_WTP_MT_PE'
+combined_dataset_name = 'CB_OC'
 df_combined_train.to_csv('sentence_level_{}_train.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
 df_combined_dev.to_csv('sentence_level_{}_dev.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
 df_combined_test.to_csv('sentence_level_{}_test.tsv'.format(combined_dataset_name), sep='\t', index=False, quotechar='"', quoting=3)
