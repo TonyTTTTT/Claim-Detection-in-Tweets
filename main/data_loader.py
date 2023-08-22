e63171ecbc0865599e9fcb217fbde4aee874a306
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2023-08-22 23:44:11 +0800

    edit the path of sentence-level dataset

--- main/data_loader.py
+++ main/data_loader.py
@@ -48,9 +48,9 @@ class DataLoader:
             self.dev_path = '../datasets/other-sentence-level/{}_processed/{}_dev.tsv'.format(dataset, dataset)
             self.test_path = '../datasets/other-sentence-level/{}_processed/{}_test.tsv'.format(dataset, dataset)
         elif dataset.startswith('sentence_level'):
-            self.train_path = '../datasets/other-sentence-level/{}_train.tsv'.format(dataset)
-            self.dev_path = '../datasets/other-sentence-level/{}_dev.tsv'.format(dataset)
-            self.test_path = '../datasets/other-sentence-level/{}_test.tsv'.format(dataset)
+            self.train_path = '../datasets/other-sentence-level/combined/{}_train.tsv'.format(dataset)
+            self.dev_path = '../datasets/other-sentence-level/combined/{}_dev.tsv'.format(dataset)
+            self.test_path = '../datasets/other-sentence-level/combined/{}_test.tsv'.format(dataset)
         elif 'GPT' in dataset:
             self.train_path = '../preprocess_datasets/preprocess_datasets_GPT/' + dataset + '_train.tsv'
             self.dev_path = '../preprocess_datasets/preprocess_datasets_GPT/' + dataset + '_dev.tsv'
